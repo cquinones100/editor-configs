76a724f1c628cad2e0ce25c498fd38cee80d1580
Carlos Quinones <cquinones100@gmail.com>
2024-05-06 12:43:57 -0400

    Add lazygit

---
+++ sync.sh
@@ -0,0 +1,5 @@
+# lazygit
+ln -s ~/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+
+# vscode
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json

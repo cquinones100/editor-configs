685fea4f7eb753a25e4f3f57efa66b882773c5ca
Carlos Quinones <cquinones100@gmail.com>
2024-05-06 12:51:33 -0400

    Add vscode
    vscode

--- sync.sh
+++ sync.sh
@@ -3,3 +3,5 @@ ln -s ~/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/confi
 
 # vscode
 ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
+ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json

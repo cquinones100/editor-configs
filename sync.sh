f7f230ff7bed4d77cc6ebebbf492b0e41a63aafc
Carlos Quinones <cquinones100@gmail.com>
2024-05-13 10:38:04 -0400

    Fix dir name

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 # lazygit
-ln -s ~/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+ln -s ~/editor-configs/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 
 # vscode
-ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/dotfiles/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
-ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/editor-configs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/editor-configs/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
+ln -s ~/editor-configs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json

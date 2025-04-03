57db63678b84a23625aa0313ff2a0968e40f4895
Carlos Quinones <cquinones100@gmail.com>
2025-04-03 09:51:23 -0400

    Code: Add support for snippets

--- sync.sh
+++ sync.sh
@@ -5,3 +5,4 @@ ln -s ~/editor-configs/lazygit/config.yml ~/Library/Application\ Support/lazygit
 ln -s ~/editor-configs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/editor-configs/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
 ln -s ~/editor-configs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/editor-configs/vscode/snippets ~/Library/Application\ Support/Code/User/snippets

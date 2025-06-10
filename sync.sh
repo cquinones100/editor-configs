11f89e9ad9408940dcdb183d0832517d9fa3661c
Carlos Quinones <cquinones100@gmail.com>
2025-06-10 09:09:31 -0400

    Make symlinks replace existing file

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 # lazygit
-ln -s ~/editor-configs/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+ln -sf ~/editor-configs/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 
 # vscode
-ln -s ~/editor-configs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/editor-configs/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
-ln -s ~/editor-configs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/editor-configs/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+ln -sf ~/editor-configs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/editor-configs/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
+ln -sf ~/editor-configs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/editor-configs/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
 
 # neovim
-ln -s ~/editor-configs/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/editor-configs/nvim/init.lua ~/.config/nvim/init.lua

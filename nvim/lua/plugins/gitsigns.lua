e72918e6dfc387db15164657a4415e90131ad61d
Carlos Quinones <cquinones100@gmail.com>
2026-03-23 10:08:24 -0400

    Add inline git blame and keymap to open blame commit on GitHub

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,26 @@
 return {
   "lewis6991/gitsigns.nvim",
-  opts = {},
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 300,
+    },
+  },
+  keys = {
+    {
+      "<leader>gp",
+      function()
+        local file = vim.fn.expand("%")
+        local line = vim.api.nvim_win_get_cursor(0)[1]
+        local result = vim.fn.system({ "git", "blame", "-L", line .. "," .. line, "--porcelain", file })
+        local sha = result:match("^(%x+)")
+        if not sha or sha:match("^0+$") then
+          vim.notify("No commit for this line (uncommitted change)", vim.log.levels.WARN)
+          return
+        end
+        vim.fn.jobstart({ "gh", "browse", sha }, { detach = true })
+      end,
+      desc = "Open blame commit on GitHub",
+    },
+  },
 }
